026f1beb5800da84421663921147c8ecb6d1ac2d
Friederike Johanna Rosa Wölke <wolke@fzp.czu.cz>
2024-06-28 19:32:57 +0200

    Merge with data from server

--- src/functions.R
+++ src/functions.R
@@ -4,7 +4,9 @@
 install_and_load <- function(package_list) {
   for (pkg in package_list) {
     if (!require(pkg, character.only = TRUE)) {
-      install.packages(pkg, dependencies = TRUE)
+      if (!is.element(pkg, installed.packages()[, "Package"])) {
+        install.packages(pkg, dependencies = TRUE)
+      }
       suppressPackageStartupMessages(library(pkg, character.only = TRUE))
     }
   }
